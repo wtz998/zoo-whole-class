c0e458944a84cbcb64a08d561ccac6463fc16b78
MichaelLu888 <40045252+MichaelLu888@users.noreply.github.com>
2019-12-19 09:00:07 -0700

    Fixed constructor issue

--- src/Duck.java
+++ src/Duck.java
@@ -8,11 +8,9 @@ public class Duck extends Avian {
      * @param endangered is the resident endangered
      * @param sleeping   is the resident sleeping
      * @param speed      speed of resident
-     * @param legs       leg count of resident
      * @param canFly     can resident fly
      */
-    public Duck(int age, String color, double weight, boolean endangered, boolean sleeping, double speed, boolean canFly, double legs) {
+    public Duck(int age, String color, double weight, boolean endangered, boolean sleeping, double speed, boolean canFly) {
         super(2, "brown and grey", 5, true, false,5, true);
-        legs = 2;
     }
 }
