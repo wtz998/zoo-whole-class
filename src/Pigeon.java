e5b6280ec5efded31645ee0b4c6da66605e8c5d2
Wensen Zhang <wensen.zhg@gmail.com>
2019-12-19 08:22:07 -0700

    update pigeon parameters

--- src/Pigeon.java
+++ src/Pigeon.java
@@ -1,18 +1,46 @@
+/**
+ * Creates a Pigeon (Avian-class) creature
+ */
 public class Pigeon extends Avian {
+
     /**
-     * builds a zoo resident
+     * Parameters for an avian creature
      *
-     * @param age        the age of the resident
-     * @param color      the color of the resident
-     * @param weight     the weight of the resident
-     * @param endangered is the resident endangered
-     * @param sleeping   is the resident sleeping
-     * @param speed
-     * @param legs
-     * @param canFly
+     * @param age        Age of creature
+     * @param color      Color of creature
+     * @param weight     Weight of creature
+     * @param endangered If creature is endangered
+     * @param sleeping   If creature is sleeping
+     * @param speed      Movement speed of creature
+     * @param canFly     If the creature can fly
      */
-    public Pigeon(int age, String color, double weight, boolean endangered, boolean sleeping, double speed, double legs, boolean canFly) {
-        super(5, "grey", 2, false, true, 35, 2, true);
+    public Pigeon(int age, String color, double weight, boolean endangered, boolean sleeping, double speed, boolean canFly) {
+        super(age, color, weight, endangered, sleeping, speed, canFly);
+    }
+
+    /**
+     * Returns information about Pigeon
+     * @return information about Pigeon
+     */
+    @Override
+    public String toString() {
+        return "ZooResident{" +
+                "age=" + super.getAge() +
+                ", color='" + super.getColor() + '\'' +
+                ", weight=" + super.getWeight() +
+                ", endangered=" + super.isEndangered() +
+                ", sleeping=" + super.isSleeping() +
+                ", speed=" + super.getSpeed() +
+                ", canFly=" + super.isCanFly() +
+                '}';
+    }
+
+    /**
+     * Entry point for Pigeon
+     * @param args
+     */
+    public static void main(String[] args) {
+        Pigeon pigeon = new Pigeon(10, "RED", 2, false, true, 20, true);
     }
 }
 
