6e6c8bf90b7d98800fef56452b7febb7af5bd496
Wensen Zhang <wensen.zhg@gmail.com>
2020-01-06 08:49:49 -0700

    Undo move

---
+++ src/avian/Duck.java
@@ -0,0 +1,16 @@
+public class Duck extends Avian {
+    /**
+     * builds a zoo resident
+     *
+     * @param age        the age of the resident
+     * @param color      the color of the resident
+     * @param weight     the weight of the resident
+     * @param endangered is the resident endangered
+     * @param sleeping   is the resident sleeping
+     * @param speed      speed of resident
+     * @param canFly     can resident fly
+     */
+    public Duck(int age, String color, double weight, boolean endangered, boolean sleeping, double speed, boolean canFly) {
+        super(2, "brown and grey", 5, true, false,5, true);
+    }
+}
---
+++ src/avian/Seagull.java
@@ -0,0 +1,19 @@
+public class Seagull extends Birbs {
+
+    /**
+     * builds a zoo resident
+     *
+     * @param age        the age of the resident
+     * @param color      the color of the resident
+     * @param weight     the weight of the resident
+     * @param endangered is the resident endangered
+     * @param sleeping   is the resident sleeping
+     */
+    public Seagull(int age, String color, double weight, boolean endangered, boolean sleeping) {
+        super(age, color, weight, endangered, sleeping);
+        
+    }
+
+
+
+}
